11f5da5318fd68caabb150fad693644229916a78
itmweb <itmwebhub@gmail.com>
2026-08-10 11:09:18 +0000

    Ejercicio 25

---
+++ itm/ejercicios-javascript/code/control-select.js
@@ -0,0 +1,7 @@
+function CambiarColor()
+{   
+document.form1.text1.value=document.form1.select1.selectedIndex;
+document.form1.text2.value=document.form1.text1.options [document.form1.select1.selectedIndex].text;
+document.form1.text3.value=document.form1.text1.options [document.form1.select1.selectedIndex].value;
+}  
+

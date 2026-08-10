c18259f296a6a7cead9e011a831bd20b8a84e7ef
Fernabe10 <157810977+Fernabe10@users.noreply.github.com>
2026-08-10 07:45:22 +0000

    Eventos en javascript

---
+++ bernabe/javascript/code/15_eventos.js
@@ -0,0 +1,11 @@
+var contador = 0;
+
+function incrementar(){
+    contador++;
+    alert("El valor de la variable contador es: " + contador);
+}
+
+function decrementar(){
+    contador--;
+    alert("El valor de la variable contador es: " + contador);
+}

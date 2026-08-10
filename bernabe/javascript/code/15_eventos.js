cece9e284543c586820d570d7408ebfa86e39015
Fernabe10 <157810977+Fernabe10@users.noreply.github.com>
2026-08-10 08:24:30 +0000

    eventos 2, mostrar contenido inputs

--- bernabe/javascript/code/15_eventos.js
+++ bernabe/javascript/code/15_eventos.js
@@ -9,3 +9,10 @@ function decrementar(){
     contador--;
     alert("El valor de la variable contador es: " + contador);
 }
+
+function mostrar(){
+    var nombre = document.form2.nombre.value;
+    var edad = document.form2.edad.value;
+
+    alert('El nombre es: ' + nombre + ' y la edad es: ' + edad);
+}

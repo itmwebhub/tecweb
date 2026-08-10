259e00696a91061ea7097bffdbb26d9ef2b10878
itmweb <itmwebhub@gmail.com>
2026-08-10 11:14:59 +0000

    Sintaxis

--- itm/ejercicios-javascript/code/control-select.js
+++ itm/ejercicios-javascript/code/control-select.js
@@ -1,7 +1,11 @@
-function CambiarColor()
-{   
-document.form1.text1.value=document.form1.select1.selectedIndex;
-document.form1.text2.value=document.form1.text1.options [document.form1.select1.selectedIndex].text;
-document.form1.text3.value=document.form1.text1.options [document.form1.select1.selectedIndex].value;
-}  
+function cambiarColor()
+{
+    document.form1.text1.value = document.form1.select1.selectedIndex;
+
+    document.form1.text2.value =
+        document.form1.select1.options[document.form1.select1.selectedIndex].text;
+
+    document.form1.text3.value =
+        document.form1.select1.options[document.form1.select1.selectedIndex].value;
+}
 

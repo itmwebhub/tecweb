0680e4982993a3119a5db4923aed5bbba5cc2838
itmweb <itmwebhub@gmail.com>
2026-08-13 07:52:11 +0000

    Eejercicio 32

---
+++ itm/ejercicios-javascript/code/objeto-ventana.js
@@ -0,0 +1,19 @@
+function cerrar()
+{
+window.close();
+}
+function abrir()
+{
+    var ventana=open();
+    ventana.document.write("Estoy escribiendo en la nueva ventana<br>")>
+    ventana.document.write("Segunda línea");
+}
+
+
+
+
+
+
+
+
+
